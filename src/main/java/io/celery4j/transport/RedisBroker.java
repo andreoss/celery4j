dbed39785c2f4d7b6d63b2f0d234f38e7d1eeef4
andreoss <andreoss@sdf.org>
2023-05-02 21:48:13 -0300

    redis adapter with timeouts and lifecycle

---
+++ src/main/java/io/celery4j/transport/RedisBroker.java
@@ -0,0 +1,149 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023
+ * SPDX-License-Identifier: MIT
+ */
+package io.celery4j.transport;
+
+import io.celery4j.protocol.JsonMessageCodec;
+import io.celery4j.protocol.Message;
+import io.celery4j.protocol.MessageCodec;
+import io.celery4j.protocol.MessageProperties;
+import io.celery4j.protocol.ProtocolException;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.exceptions.JedisException;
+
+/**
+ * A broker backed by a key value store that carries each queue as a list.
+ *
+ * <p>A message is pushed at the head of the list and read from its tail, so
+ * that messages leave in the order they arrived. The queue a message goes to
+ * is the one its priority names, which is how this transport carries
+ * priorities it has none of its own.</p>
+ *
+ * <p>The pool is supplied rather than opened here, so that a caller decides
+ * how many connections there are and how they are configured. Closing this
+ * broker closes that pool.</p>
+ *
+ * @since 0.1.0
+ */
+public final class RedisBroker implements Broker {
+
+    /**
+     * Codec used when none was given.
+     */
+    private static final MessageCodec CODEC = new JsonMessageCodec();
+
+    /**
+     * Queue naming used when none was given.
+     */
+    private static final Queues NAMES = new Queues();
+
+    /**
+     * Pool the connections come from.
+     */
+    private final JedisPool pool;
+
+    /**
+     * Codec the messages are read and written with.
+     */
+    private final MessageCodec codec;
+
+    /**
+     * Names a queue is known by.
+     */
+    private final Queues queues;
+
+    /**
+     * Ctor.
+     *
+     * @param pool Pool the connections come from
+     */
+    public RedisBroker(final JedisPool pool) {
+        this(pool, RedisBroker.CODEC, RedisBroker.NAMES);
+    }
+
+    /**
+     * Ctor.
+     *
+     * @param pool Pool the connections come from
+     * @param codec Codec the messages are read and written with
+     * @param queues Names a queue is known by
+     */
+    public RedisBroker(final JedisPool pool, final MessageCodec codec, final Queues queues) {
+        this.pool = pool;
+        this.codec = codec;
+        this.queues = queues;
+    }
+
+    @Override
+    public void send(final Message message, final String queue) throws TransportException {
+        try (Jedis jedis = this.pool.getResource()) {
+            jedis.lpush(
+                RedisBroker.key(
+                    this.queues.named(
+                        queue, message.properties().number(MessageProperties.PRIORITY, 0L)
+                    )
+                ),
+                this.written(message)
+            );
+        } catch (final JedisException ex) {
+            throw new TransportException(String.format("queue %s cannot be written", queue), ex);
+        }
+    }
+
+    @Override
+    public Optional<Message> receive(final String queue, final Duration timeout)
+        throws TransportException {
+        final List<byte[]> popped;
+        try (Jedis jedis = this.pool.getResource()) {
+            popped = jedis.brpop(RedisBroker.seconds(timeout), RedisBroker.key(queue));
+        } catch (final JedisException ex) {
+            throw new TransportException(String.format("queue %s cannot be read", queue), ex);
+        }
+        final Optional<Message> message;
+        if (popped == null || popped.size() < 2) {
+            message = Optional.empty();
+        } else {
+            message = Optional.of(this.read(popped.get(1)));
+        }
+        return message;
+    }
+
+    @Override
+    public void close() throws TransportException {
+        try {
+            this.pool.close();
+        } catch (final JedisException ex) {
+            throw new TransportException("broker cannot be closed", ex);
+        }
+    }
+
+    private byte[] written(final Message message) {
+        try {
+            return this.codec.encode(message);
+        } catch (final ProtocolException ex) {
+            throw new TransportException("message cannot be written", ex);
+        }
+    }
+
+    private Message read(final byte[] raw) {
+        try {
+            return this.codec.decode(raw);
+        } catch (final ProtocolException ex) {
+            throw new TransportException("message cannot be read", ex);
+        }
+    }
+
+    private static byte[] key(final String name) {
+        return name.getBytes(StandardCharsets.UTF_8);
+    }
+
+    private static double seconds(final Duration timeout) {
+        return Math.max(0.001, timeout.toMillis() / (double) 1000L);
+    }
+}
---
+++ src/test/java/io/celery4j/transport/RedisBrokerIT.java
@@ -0,0 +1,86 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023
+ * SPDX-License-Identifier: MIT
+ */
+package io.celery4j.transport;
+
+import io.celery4j.protocol.Message;
+import io.celery4j.protocol.MessageProperties;
+import java.time.Duration;
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import redis.clients.jedis.JedisPool;
+
+/**
+ * Live test case for {@link RedisBroker}, against a store in a container.
+ *
+ * <p>The store announces the address it was given rather than being assumed at
+ * a port, so that runs beside each other cannot collide.</p>
+ *
+ * @since 0.1.0
+ */
+@Tag("live")
+@Testcontainers
+final class RedisBrokerIT implements BrokerContract {
+
+    /**
+     * Port the store listens on inside its container.
+     */
+    private static final int PORT = 6379;
+
+    /**
+     * The store the cases run against.
+     */
+    @Container
+    private static final GenericContainer<?> STORE =
+        new GenericContainer<>("docker.io/library/redis:7-alpine")
+            .withExposedPorts(RedisBrokerIT.PORT);
+
+    @Override
+    public Broker broker() {
+        return new RedisBroker(
+            new JedisPool(
+                RedisBrokerIT.STORE.getHost(),
+                RedisBrokerIT.STORE.getMappedPort(RedisBrokerIT.PORT)
+            )
+        );
+    }
+
+    @Override
+    public String queue() {
+        return "live";
+    }
+
+    @Test
+    void sendsAPriorityToItsOwnQueue() {
+        try (Broker broker = this.broker()) {
+            final String queue = this.queue("priority");
+            final Message plain = this.message("proj.tasks.add", queue);
+            broker.send(
+                new Message(
+                    plain.properties().with(MessageProperties.PRIORITY, 6),
+                    plain.headers(),
+                    plain.body()
+                ),
+                queue
+            );
+            Assertions.assertTrue(
+                broker.receive(new Queues().named(queue, 6L), Duration.ofSeconds(2)).isPresent()
+            );
+        }
+    }
+
+    @Test
+    void reportsAnEmptyQueueWithinItsTimeout() {
+        try (Broker broker = this.broker()) {
+            Assertions.assertEquals(
+                Optional.empty(), broker.receive(this.queue("timeout"), Duration.ofMillis(300))
+            );
+        }
+    }
+}
